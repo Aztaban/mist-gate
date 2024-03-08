ed7388ef6efb1444c7f7cb7f85d7476e34545e91
Martin Justa <Aztabanee@gmail.com>
2024-03-08 03:33:25 +0100

    first components and basic style added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
-  return <div></div>;
+  const content = (
+    <>
+      <Header />
+      <main className='main'>
+        <p>main</p>
+      </main>
+      <Footer />
+    </>
+  );
+
+  return content;
 }
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+
+
+const Nav = () => {
+  return (
+    <div>
+      Navigation
+    </div>
+  )
+}
+
+export default Nav
