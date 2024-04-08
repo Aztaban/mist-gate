dff3732bae0599ae6bfa33d3565cde6d15447292
Martin Justa <Aztabanee@gmail.com>
2024-04-09 00:59:42 +0200

    mod authApiSlice, new endpoints added

--- src/features/auth/authApiSlice.ts
+++ src/features/auth/authApiSlice.ts
@@ -1,30 +1,69 @@
-import { apiSlice } from "../../app/api/apiSlice";
+import { apiSlice } from '../../app/api/apiSlice';
+import { logOut, setCredentials } from './authSlice';
 
 export interface LoginCredentialsType {
   user: string;
   pwd: string;
 }
 
+interface AuthResponse {
+  accessToken: string;
+}
+
 export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    login: builder.mutation<LoginCredentialsType, string>({
+    login: builder.mutation<AuthResponse, LoginCredentialsType>({
       query: (credentials) => ({
         url: '/auth',
         method: 'POST',
-        body: {...credentials}
-      })
+        body: { ...credentials },
+      }),
     }),
     register: builder.mutation<void, LoginCredentialsType>({
       query: (newLogin) => ({
         url: '/auth/register',
         method: 'POST',
-        body: newLogin
-      })
-    })
-  })
-})
+        body: newLogin,
+      }),
+    }),
+    sendLogout: builder.mutation<void, void>({
+      query: () => ({
+        url: '/auth/logout',
+        method: 'POST',
+      }),
+      async onQueryStarted(_arg, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled;
+          dispatch(logOut());
+          setTimeout(() => {
+            dispatch(apiSlice.util.resetApiState());
+          }, 1000);
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    }),
+    refresh: builder.mutation<AuthResponse, void>({
+      query: () => ({
+        url: '/auth/refresh',
+        method: 'GET',
+      }),
+      async onQueryStarted(_arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          const { accessToken } = data;
+          dispatch(setCredentials({ accessToken }));
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    }),
+  }),
+});
 
 export const {
   useLoginMutation,
-  useRegisterMutation
-} = authApiSlice
+  useRegisterMutation,
+  useSendLogoutMutation,
+  useRefreshMutation,
+} = authApiSlice;
--- src/features/shop/productSlice.ts
+++ src/features/shop/productSlice.ts
@@ -1,4 +1,4 @@
-import { apiSlice } from '../api/apiSlice';
+import { apiSlice } from "../../app/api/apiSlice";
 
 export interface ProductType {
   id: string;
