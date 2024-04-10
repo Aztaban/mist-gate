81585688150425d91da73e59ed37fe0a75c2a854
Martin Justa <Aztabanee@gmail.com>
2024-04-11 00:49:00 +0200

    add RequireAuth, role Authentication component

---
+++ src/components/auth/RequireAuth.tsx
@@ -0,0 +1,22 @@
+
+import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+
+interface RequiredAuthProps {
+  allowedRoles: number[]
+}
+
+const RequireAuth: React.FC<RequiredAuthProps> = ({ allowedRoles }) => {
+  const location = useLocation();
+  const { roles } = useAuth();
+
+  const content = roles.some((role) => allowedRoles.includes(role)) ? (
+    <Outlet /> 
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+
+  return content;
+}
+
+export default RequireAuth;
