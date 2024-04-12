00a304e5c851e30259aeee21ede80ed743ecacfc
Martin Justa <Aztabanee@gmail.com>
2024-04-12 14:09:20 +0200

    add Login components, incomplete

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,4 +1,55 @@
+import { useRef, useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setCredentials } from '../../features/auth/authSlice';
+import { useLoginMutation } from '../../features/auth/authApiSlice';
+import usePersist from '../../hooks/usePersist';
+
 const Login = () => {
+  const userRef = useRef<HTMLInputElement>(null);
+  const errRef = useRef<HTMLParagraphElement>(null);
+
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [errMsg, setErrMsg] = useState<string>('')
+  const [persist, setPersist] = usePersist();
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  useEffect(() => {
+    if (userRef.current) userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [username, password]);
+
+  const errClass = errMsg ? 'errmsg' : 'offscreen';
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const { accessToken } = await login({ user, pwd }).unwrap();
+      dispatch(setCredentials({ accessToken }));
+      setUsername('');
+      setPassword('');
+      navigate('/dash');
+    } catch (error: any) {
+      if (!error.status) {
+        setErrMsg('No Server Response');
+      } else if (error.status === 400) {
+        setErrMsg('Missing Username or Password');
+      } else if (error.status === 401) {
+        setErrMsg('Unauthorized');
+      } else {
+        setErrMsg(error.data?.message);
+      }
+    }
+  }
+
   return (
     <main>
       <h2>Login</h2>
