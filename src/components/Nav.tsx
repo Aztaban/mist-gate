6b883b25bae978527caee0ba28d88fe4635e2eb5
Martin Justa <Aztabanee@gmail.com>
2024-04-23 15:47:47 +0200

    navbar now changes in dependency of logedIn status

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const Nav = () => {
+  const { isLogedIn } = useAuth();
+
+
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="about">About</Link></li>
-        <li><Link to="shop">Shop</Link></li>
-        <li><Link to="shop/cart">Cart</Link></li>
-        <li><Link to="login">Login</Link></li>
-        <li><Link to="account">Account</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="about">About</Link>
+        </li>
+        <li>
+          <Link to="shop">Shop</Link>
+        </li>
+        <li>
+          <Link to="shop/cart">Cart</Link>
+        </li>
+        {isLogedIn ? (
+          <li>
+            <Link to="account">Account</Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="login">Login</Link>
+          </li>
+        )}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
