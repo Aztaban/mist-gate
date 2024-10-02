81808a2307053b50bcfaf4e4b38e3b7a6bb3c208
Martin Justa <Aztabanee@gmail.com>
2024-10-02 15:13:06 +0200

    SImplified logic

--- src/components/auth/RequireAuth.tsx
+++ src/components/auth/RequireAuth.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement } from 'react';
 import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import { useSelector } from 'react-redux';
@@ -14,20 +14,6 @@ const RequireAuth: React.FC<RequiredAuthProps> = ({
   const location = useLocation();
   const { roles } = useAuth();
   const token = useSelector(selectCurrentToken);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Check if token exists and if allowedRoles include user's roles
-    if (token && roles.some((role) => allowedRoles.includes(role))) {
-      setLoading(false);
-    } else {
-      setLoading(true);
-    }
-  }, [token, roles, allowedRoles]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
   // If token doesn't exist, redirect to login
   if (!token) {
@@ -35,13 +21,11 @@ const RequireAuth: React.FC<RequiredAuthProps> = ({
   }
 
   // If user doesn't have required roles, redirect to login with from location
-  const content = roles.some((role) => allowedRoles.includes(role)) ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
+  if (!roles.some((role) => allowedRoles.includes(role))) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
-  return content;
+  return <Outlet />;
 };
 
 export default RequireAuth;
