20e06820f06756402df5bb93f208f8dafe747e1f
Martin Justa <Aztabanee@gmail.com>
2024-10-03 04:27:53 +0200

    Added Mist component for background Mist effect.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
 import Footer from "./Footer"
+import Mist from "./Mist"
 
 const Layout = () => {
   return (
     <>
+      <Mist />
       <Header />
       <main className="app">
         <Outlet />
---
+++ src/components/Mist.tsx
@@ -0,0 +1,13 @@
+const Mist = () => {
+  return (
+      <section className="fog">
+        <div className="absolute-bg"></div>
+        <div className="fog-container">
+          <div className="fog-img fog-img-first"></div>
+          <div className="fog-img fog-img-second"></div>
+        </div>
+      </section>
+  );
+};
+
+export default Mist;
