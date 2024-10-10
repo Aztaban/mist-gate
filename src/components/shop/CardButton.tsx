4ecf2f71630244f1a79f0d6c6a166b37991b767c
Martin Justa <Aztabanee@gmail.com>
2024-10-10 05:14:11 +0200

    Added items counter

--- src/components/shop/CardButton.tsx
+++ src/components/shop/CardButton.tsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from 'react-redux';
+import { selectCartItems } from '../../features/cart/cartSlice';
 
-interface CartButtonProps {
-  cartItemCount: number;
-}
+const CartButton = () => {
+  const cartItems = useSelector(selectCartItems);
+  const totalQuantity = cartItems.reduce((total, item) => total + item.qty, 0);
 
-const CartButton = ({ cartItemCount }: CartButtonProps) => {
   return (
     <Link to="/shop/cart" className="cart__link">
-      <span className="cart__btn" data-count={cartItemCount}>
+      <span className="cart__btn" data-count={totalQuantity}>
         <FontAwesomeIcon icon={faShoppingCart} />
       </span>
     </Link>
