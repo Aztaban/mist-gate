d8a9e88a54466e8d7f32cffe84a9c589ae781202
Aztaban <Aztabanee@gmail.com>
2024-10-19 23:39:44 +0200

    Added Orders pull.

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useGetAllOrdersQuery } from '../../features/shop/ordersSlice'
 
 const Admin = () => {
+  const {data, isLoading, isError } = useGetAllOrdersQuery();
+
+  if (isLoading) {
+    return <p>Orders are loading...</p>
+  }
+
+  if (isError) {
+    return <p>Error loading orders.</p>
+  }
+
+  console.log(data);
+
   return (
     <div>
       Admin
