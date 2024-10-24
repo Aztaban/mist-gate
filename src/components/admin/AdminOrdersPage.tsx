ae90ee9324e897abb8a15b49ffa8a2aa2322744c
Martin Justa <Aztabanee@gmail.com>
2024-10-25 01:21:43 +0200

    feat: add new admin components in navigation

---
+++ src/components/admin/AdminOrdersPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminOrdersPage = () => {
+  return (
+    <div>
+      <h2>Admin Orders</h2>
+    </div>
+  )
+}
+
+export default AdminOrdersPage
---
+++ src/components/admin/AdminProductsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminProductsPage = () => {
+  return (
+    <div>
+      <h2>Admin Products</h2>
+    </div>
+  )
+}
+
+export default AdminProductsPage
