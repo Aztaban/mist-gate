648fc224abea9c949a56b73e8b6113f5fe12e63a
Martin Justa <Aztabanee@gmail.com>
2024-10-26 01:42:16 +0200

    feat: add OrderLineItem component

---
+++ src/components/checkout/OrderLineItem.tsx
@@ -0,0 +1,25 @@
+import { ReactElement } from "react";
+import { NavLink } from "react-router-dom";
+import { Order } from "../../features/shop/ordersSlice";
+
+interface OrderLineItemProps {
+  order: Order;
+}
+
+const OrderLineItem = ({ order }: OrderLineItemProps): ReactElement => {
+  return (
+    <div className="order-line-item">
+      <NavLink to="/order">{order.orderNo}</NavLink>
+
+      <span className="order-date">
+        Created on: {new Date(order.created_at).toLocaleDateString()}
+      </span>
+
+      <span className={`order-status ${order.paidAt ? 'paid' : 'not-paid'}`}>
+        {order.paidAt ? 'Paid' : 'Not Paid'}
+      </span>
+    </div>
+  )
+}
+
+export default OrderLineItem;
