f7063a425fb3e1f0fb99a622e367467b3c5c4c2a
Martin Justa <Aztabanee@gmail.com>
2024-10-27 00:35:15 +0200

    style(order)

--- src/components/checkout/SingleOrderPage.tsx
+++ src/components/checkout/SingleOrderPage.tsx
@@ -18,19 +18,26 @@ const SingleOrderPage = () => {
 
   const handleBackBtn = () => {
     navigate(-1);
-  }
+  };
 
   return (
-    <article>
-      <header>
+    <article className="order">
+      <header className="orders-header">
         <h2>Order Details</h2>
       </header>
       <main>
         <h3>Order No: {order.orderNo}</h3>
-        <p>Created on: {new Date(order.created_at).toLocaleDateString()}</p>
-        <p>Order status: {order.status}</p>
-        <p>Paid status: {order.isPaid ? "Paid" : "Not Paid"}</p>
-        <div>
+        <div className="order-field">
+          <p>Created on:</p>
+          <p>{new Date(order.created_at).toLocaleDateString()}</p>
+          <p>Order status:</p>
+          <p>{order.status.toUpperCase()}</p>
+          <p>Shipping Method:</p>
+          <p>{order.shippingMethod ? order.shippingMethod : 'No data'}</p>
+          <p>Paid status:</p>
+          <p>{order.isPaid ? 'Paid' : 'Not Paid'}</p>
+        </div>
+        <div className="order-products">
           <h3>Products</h3>
           <ul>
             {order.products.map((product) => (
@@ -40,23 +47,30 @@ const SingleOrderPage = () => {
               </li>
             ))}
           </ul>
-          <div>
-          <p>Items Price: {eurFormat(order.itemsPrice)}</p>
-          <p>Shipping Price: {eurFormat(order.shippingPrice)}</p>
-          <p>Price without Taxes: {eurFormat(countTaxFree(order.totalPrice))}</p>
-          <p>Total Price: {eurFormat(order.totalPrice)}</p>
-        </div>
+          <div className="order-field">
+            <p>Items Price:</p>
+            <p>{eurFormat(order.itemsPrice)}</p>
+            <p>Shipping Price:</p>
+            <p>{eurFormat(order.shippingPrice)}</p>
+            <p>Price without Taxes:</p>
+            <p>{eurFormat(countTaxFree(order.totalPrice))}</p>
+            <p>Total Price:</p>
+            <p>{eurFormat(order.totalPrice)}</p>
+          </div>
         </div>
         <div>
           <h3>Address</h3>
-          <p>{address}</p>
-          <p>
-            {city}, {postalCode}
-          </p>
-          <p>{country}</p>
+          <div className="order-field">
+              <p>Address:</p><p>{address}</p>
+              <p>City, postal code:</p><p>{city}, {postalCode}</p>
+              <p>Country: </p><p>{country}</p>
+
+          </div>
         </div>
 
-        <button className='btn back-btn' onClick={handleBackBtn}>Go Back</button>
+        <button className="btn back-btn" onClick={handleBackBtn}>
+          Go Back
+        </button>
       </main>
     </article>
   );
