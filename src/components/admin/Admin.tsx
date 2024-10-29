fcf03990e22821867b5f25d1ca3d6e6340ac1458
Martin Justa <Aztabanee@gmail.com>
2024-10-29 23:41:46 +0100

    Feat(Admin): added orders fetch

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,23 +1,34 @@
-import { useGetAllOrdersQuery } from '../../features/shop/ordersSlice'
+import { useGetAllOrdersQuery } from '../../features/shop/ordersApiSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectOrders, setOrders } from '../../features/shop/ordersSlice';
+import { useEffect } from 'react';
 
 const Admin = () => {
-  const {data, isLoading, isError } = useGetAllOrdersQuery();
+  const dispatch = useDispatch();
+  const { data: ordersData, isError, isLoading } = useGetAllOrdersQuery();
+  const orders = useSelector(selectOrders);
 
-  if (isLoading) {
-    return <p>Orders are loading...</p>
-  }
+  useEffect(() => {
+    if (ordersData) {
+      dispatch(setOrders(ordersData));
+    }
+  }, [ordersData, dispatch]);
 
-  if (isError) {
-    return <p>Error loading orders.</p>
-  }
-
-  console.log(data);
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching data</div>;
 
   return (
-    <div>
-      Admin
-    </div>
-  )
-}
+    <article className="account">
+      <h2 className="orders-header">Admin Dashboard</h2>
+      <ul>
+        {orders.map((order) => (
+          <li key={order.id}>
+            Order #{order.orderNo} - Total: {order.totalPrice}
+          </li>
+        ))}
+      </ul>
+    </article>
+  );
+};
 
-export default Admin
+export default Admin;
