f9990e2576cc0242efeb515e59a5a7eaee30cc09
Martin Justa <Aztabanee@gmail.com>
2024-10-29 23:42:49 +0100

    Feat add new component

--- src/components/admin/AdminOrdersPage.tsx
+++ src/components/admin/AdminOrdersPage.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectOrders } from '../../features/shop/ordersSlice';
+import { Order } from '../../features/shop/ordersApiSlice';
+import SearchBar from './SearchBar';
+import AdminOrderList from './AdminOrderList';
 
 const AdminOrdersPage = () => {
-  return (
-    <div>
-      <h2>Admin Orders</h2>
-    </div>
-  )
-}
+  const orders = useSelector(selectOrders);
 
-export default AdminOrdersPage
+  const [filteredOrders, setFilteredOrders] = useState<Order[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const handleSearchChange = (term: string) => {
+    setSearchTerm(term);
+
+    const lowerCasedTerm = term.toLowerCase();
+    const newFilteredOrders = orders.filter(
+      (order: Order) =>
+        order.id.toString().includes(lowerCasedTerm) ||
+        order.user.username.toLowerCase().includes(lowerCasedTerm)
+    );
+    setFilteredOrders(newFilteredOrders);
+  };
+
+  return (
+    <article className="order">
+      <h2 className='orders-header'>Admin Orders</h2>
+      <SearchBar  />
+      <AdminOrderList orders={orders}/>
+
+    </article>
+  );
+};
+
+export default AdminOrdersPage;
