0e33c5e5bd8ccb66673f12997cb29c39b7d3627e
Martin Justa <Aztabanee@gmail.com>
2024-10-30 01:50:47 +0100

    fead: add pagination component

---
+++ src/components/common/Pagination.tsx
@@ -0,0 +1,45 @@
+import { ReactElement } from 'react';
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationProps): ReactElement => {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  const handlePrevious = () => {
+    if (currentPage > 1) onPageChange(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) onPageChange(currentPage + 1);
+  };
+
+  return (
+    <div className="pagination">
+      <button onClick={handlePrevious} disabled={currentPage === 1}>
+        Previous
+      </button>
+      {pages.map((page) => (
+        <button
+          key={page}
+          onClick={() => onPageChange(page)}
+          disabled={page === currentPage}
+        >
+          {page}
+        </button>
+      ))}
+      <button onClick={handleNext} disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
