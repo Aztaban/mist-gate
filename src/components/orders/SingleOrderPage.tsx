3572e7879cbd753097d74ab1331b0d79c48352c9
Martin Justa <Aztabanee@gmail.com>
2024-11-01 15:07:54 +0100

    feat add admin tools

--- src/components/orders/SingleOrderPage.tsx
+++ src/components/orders/SingleOrderPage.tsx
@@ -1,23 +1,62 @@
 import { useParams, useNavigate } from 'react-router-dom';
-import { useGetOrderByIdQuery } from '../../features/shop/ordersApiSlice';
+import { useState, ChangeEvent } from 'react';
+import {
+  useGetOrderByIdQuery,
+  useUpdateOrderShippingMutation,
+  useUpdateOrderStatusMutation,
+} from '../../features/shop/ordersApiSlice';
+import useAuth from '../../hooks/useAuth';
+import { OrderStatus } from '../../config/orderStatus';
+import { ShippingMethod } from '../../config/shippingConfig';
 import OrderProducts from './OrderProducts';
 import OrderPriceSummary from './OrderPriceSummary';
 import Address from './Address';
 
 const SingleOrderPage = () => {
   const navigate = useNavigate();
+  const { isAdmin } = useAuth();
   const { orderId } = useParams<{ orderId: string }>();
   const {
     data: order,
     isLoading,
     isError,
   } = useGetOrderByIdQuery(orderId ? orderId : '');
+  const [updateOrderStatus] = useUpdateOrderStatusMutation();
+  const [updateOrderShipping] = useUpdateOrderShippingMutation();
+  const [status, setStatus] = useState<OrderStatus | ''>(order?.status || '');
+  const [shippingMethod, setShippingMethod] = useState<ShippingMethod | ''>(
+    order?.shippingMethod || ''
+  );
 
   if (isLoading) return <p>Loading Order...</p>;
   if (isError || !order) return <p>Order not found.</p>;
 
   const handleBackBtn = () => {
     navigate(-1);
+  };
+
+  const handleStatusChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setStatus(e.target.value as OrderStatus);
+  };
+
+  const handleShippingMethodChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setShippingMethod(e.target.value as ShippingMethod);
+  };
+
+  const handleStatusSave = async () => {
+    try {
+      if (orderId && status && status != order.status) {
+        await updateOrderStatus({ orderId, status }).unwrap();
+      }
+
+      if (orderId && shippingMethod && shippingMethod != order.shippingMethod) {
+        await updateOrderShipping({ orderId, shippingMethod }).unwrap();
+      }
+
+      alert('Order status has been updated successfully');
+    } catch (error) {
+      alert('Failed to update order status');
+    }
   };
 
   return (
@@ -54,9 +93,43 @@ const SingleOrderPage = () => {
           totalPrice={order.totalPrice}
         />
         <Address address={order.shippingAddress} />
+
+        {/* Admin-only tools */}
+        {isAdmin && (
+          <section className="admin-tools">
+            <h3>Admin Tools</h3>
+
+            <div>
+              <label htmlFor="status">Update Status:</label>
+              <select id="status" value={status} onChange={handleStatusChange}>
+                {Object.values(OrderStatus).map((status) => (
+                  <option key={status} value={status}>
+                    {status.charAt(0).toUpperCase() + status.slice(1)}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div>
+              <label htmlFor="shippingMethod">Update Shipping Method:</label>
+              <select
+                id="shippingMethod"
+                value={shippingMethod}
+                onChange={handleShippingMethodChange}
+              >
+                {Object.values(ShippingMethod).map((method) => (
+                  <option key={method} value={method}>
+                    {method.charAt(0).toUpperCase() + method.slice(1)} Shipping
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <button onClick={handleStatusSave}>Save Changes</button>
+          </section>
+        )}
       </main>
       <div className="buttons-box">
-        {' '}
         <button className="btn back-btn" onClick={handleBackBtn}>
           Go Back
         </button>
