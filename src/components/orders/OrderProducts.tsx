b007402ec817f03f5f4d23032c7d522cdcdb23db
Martin Justa <Aztabanee@gmail.com>
2024-11-06 23:31:16 +0100

    fix: correct path for OrderItem

--- src/components/orders/OrderProducts.tsx
+++ src/components/orders/OrderProducts.tsx
@@ -1,4 +1,4 @@
-import { OrderItem } from '../../features/shop/ordersSlice';
+import { OrderItem } from '../../features/shop/ordersApiSlice';
 import OrderProductLineItem from './OrderProductLineItem';
 import { eurFormat } from '../../utils/utils';
 
