5cbb8dffdd83a68597ce0e8e978dbfce72eb753f
Martin Justa <Aztabanee@gmail.com>
2024-12-03 06:05:40 +0100

    feat: add useLogout hook

---
+++ src/hooks/useLogout.ts
@@ -0,0 +1,18 @@
+import { useSendLogoutMutation } from '../features/auth/authApiSlice';
+import { setPersistState } from '../utils/utils';
+
+export const useLogout = () => {
+  const [sendLogout] = useSendLogoutMutation();
+
+  const logout = () => {
+    if (window.confirm('Are you sure you want log out?')) {
+      setPersistState(false);
+      sendLogout();
+      document.cookie =
+        'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=localhost:5173; secure; HttpOnly';
+      window.location.href = '/login';
+    }
+  };
+
+  return { logout };
+};
