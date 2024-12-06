a7f1850b06ca70dcc31170f875feb7385126099d
Martin Justa <Aztabanee@gmail.com>
2024-12-06 19:22:55 +0100

    fix: show menu on high resolution.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,7 +12,6 @@ const Header = () => {
   const menuRef = useRef<HTMLElement>(null);
 
   const toggleMenu = () => setIsMenuOpen((prev) => !prev);
-  const closeMenu = () => setIsMenuOpen(false);
 
   const handleOutsideClick = (event: MouseEvent | TouchEvent) => {
     if (
