9416933bb7a4aeeb10e3b491a8b0ea8aa4c6c286
Martin Justa <Aztabanee@gmail.com>
2024-12-09 04:53:34 +0100

    refactor: simplified OrderSummary.

--- src/components/checkout/OrderSummary.tsx
+++ src/components/checkout/OrderSummary.tsx
@@ -1,64 +1,25 @@
-import { useLocation, useNavigate } from 'react-router-dom';
 import { CreateOrder } from '../../features/shop/ordersApiSlice';
-import { useAddNewOrderMutation } from '../../features/shop/ordersApiSlice';
-import { clearCart } from '../../features/cart/cartSlice';
-import { useDispatch } from 'react-redux';
 import OrderProducts from '../orders/OrderProducts';
 import Address from '../orders/Address';
 import OrderPriceSummary from '../orders/OrderPriceSummary';
 
-const OrderSummary = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const order: CreateOrder = location.state;
+interface OrderSummaryProps {
+  order: CreateOrder;
+}
 
+const OrderSummary = ({ order }: OrderSummaryProps) => {
   const totalPrice = order.itemsPrice + order.shippingPrice;
-
-  const [addNewOrder, { isSuccess, isError, error }] = useAddNewOrderMutation();
-
-  const handleSubmitOrder = async (e: React.MouseEvent) => {
-    e.preventDefault();
-
-    try {
-      const newOrderId = await addNewOrder(order).unwrap();
-      alert('Order created successfully!');
-      dispatch(clearCart());
-      navigate(`/order/${newOrderId}`);
-    } catch (err) {
-      console.error('failed to create order', err);
-    }
-  };
-
-  const navigateBack = () => {
-    navigate(-1);
-  };
 
   return (
     <>
-      <article className="order">
-        <h2 className="orders-header">Order Summary</h2>
-        <main>
-          <Address address={order.shippingAddress} />
-          <h3>Products</h3>
-          <OrderProducts products={order.products} />
-          <h3>Order Summary</h3>
-          <OrderPriceSummary
-            itemsPrice={order.itemsPrice}
-            shippingPrice={order.shippingPrice}
-            totalPrice={totalPrice}
-          />
-          {isSuccess && <p>Order created successfully!</p>}
-          {isError && <p>Error: {error.toString()}</p>}
-        </main>
-        <div className="buttons-box">
-          <button className="btn back-btn" onClick={navigateBack}>
-            Back to Address
-          </button>
-          <button className="btn save-btn" onClick={handleSubmitOrder}>
-            Confirm Order
-          </button>
-        </div>
+      <article className="order-summary">
+        <Address address={order.shippingAddress} />
+        <OrderProducts products={order.products} />
+        <OrderPriceSummary
+          itemsPrice={order.itemsPrice}
+          shippingPrice={order.shippingPrice}
+          totalPrice={totalPrice}
+        />
       </article>
     </>
   );
