a097af9c2661a8c040f53b3f8b7cbc6783f2aaae
Martin Justa <Aztabanee@gmail.com>
2024-12-11 07:48:39 +0100

    refactor: All copoments connected and flow works.

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -4,42 +4,68 @@ import { useNavigate } from 'react-router-dom';
 import Cart from './Cart';
 import ShippingAndAddress from './ShippingAndAddress';
 import OrderSummary from './OrderSummary';
+import OrderConfirmation from './OrderConfirmation';
 import { useAddNewOrderMutation } from '../../features/shop/ordersApiSlice';
-import { CreateOrder } from '../../features/shop/ordersApiSlice';
-import { selectCreateOrder } from '../../features/cart/cartSlice';
-import { useSelector } from 'react-redux';
+import { clearCart } from '../../features/checkout/checkoutSlice';
+import { useDispatch } from 'react-redux';
+import { useValidateOrder } from '../../hooks/useValidateOrder';
 
 const Checkout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { isLogedIn } = useAuth();
   const [step, setStep] = useState<number>(1);
-  const [newOrder, setNewOrder] = useState<CreateOrder | null>(null);
-  //const [createOrderMutation] = useAddNewOrderMutation();
+  const [orderId, setOrderId] = useState<string>('test');
+  const [validateAddress, setValidateAddress] = useState<boolean>(false);
 
-  const order = useSelector(selectCreateOrder);
+  const [addNewOrderMutation] = useAddNewOrderMutation();
+  const { isValid, isCart, orderData } = useValidateOrder();
 
   const steps = [
     { title: 'Cart', content: <Cart />, button: 'Continue to address' },
     {
       title: 'Address and Shipping',
-      content: <ShippingAndAddress/>,
+      content: (
+        <ShippingAndAddress
+          validateFields={validateAddress}
+          setValidateFields={setValidateAddress}
+        />
+      ),
       button: 'Order Summary',
     },
     {
       title: 'Order Summary',
-      content: newOrder ? <OrderSummary order={newOrder}/> : null,
+      content: orderData ? <OrderSummary order={orderData} /> : null,
       button: 'Confirm Order',
+    },
+    {
+      title: 'Payment',
+      content: orderId ? <OrderConfirmation orderId={orderId} /> : null,
+      button: 'Go to Payment',
     },
   ];
 
-  const handleNext = () => {
+  const handleNext = async () => {
     if (!isLogedIn) navigate('/login');
-    if (step === 2 && order) {
-      setNewOrder(order);
-      setStep((prev) => prev + 1);
-    } else if (step < steps.length) {
-      setStep((prev) => prev + 1);
+    if (step === 2 && !validateAddress && !isValid) {
+      setValidateAddress(true);
+      return;
     }
+    if (step === 3 && orderData) {
+      try {
+        const result = await addNewOrderMutation(orderData).unwrap();
+        console.log(result);
+        setOrderId(result);
+        dispatch(clearCart())
+      } catch {
+        console.log('Order failed to create!');
+        return;
+      }
+    }
+    if (step === 4 && orderId) {
+      navigate(`/checkout/payment/${orderId}`);
+    }
+    setStep((prev) => prev + 1);
   };
 
   const handlePrevious = () => {
@@ -55,7 +81,7 @@ const Checkout = () => {
       </h2>
       {currentStep.content}
       <div className="checkout-buttons">
-        {step > 1 ? (
+        {1 < step && step < 4 ? (
           <button
             className="btn save-btn"
             onClick={handlePrevious}
@@ -69,7 +95,7 @@ const Checkout = () => {
         <button
           className="btn back-btn"
           onClick={handleNext}
-          disabled={step === 3 || (step === 2 && !order)}
+          disabled={!isCart}
         >
           {currentStep.button}
         </button>
