79272becf1b721f78e3af3c63059fb400e176035
Martin Justa <Aztabanee@gmail.com>
2024-12-12 05:11:13 +0100

    refactor: cleanup in component.

--- src/components/orders/SingleOrderPage.tsx
+++ src/components/orders/SingleOrderPage.tsx
@@ -1,139 +1,30 @@
-import { useParams, useNavigate } from 'react-router-dom';
-import { useState, ChangeEvent } from 'react';
-import {
-  useGetOrderByIdQuery,
-  useUpdateOrderShippingMutation,
-  useUpdateOrderStatusMutation,
-} from '../../features/shop/ordersApiSlice';
-import useAuth from '../../hooks/useAuth';
-import { OrderStatus } from '../../config/orderStatus';
-import { ShippingMethod } from '../../config/shippingConfig';
+import { useParams } from 'react-router-dom';
+import useOrder from '../../hooks/useOrder';
 import OrderProducts from './OrderProducts';
+import OrderSummary from './OrderSummary';
 import OrderPriceSummary from './OrderPriceSummary';
 import Address from './Address';
+import OrderActions from '../admin/orders/OrderActions';
 
 const SingleOrderPage = () => {
-  const navigate = useNavigate();
-  const { isAdmin } = useAuth();
-  const { orderId } = useParams<{ orderId: string }>();
-  const {
-    data: order,
-    isLoading,
-    isError,
-  } = useGetOrderByIdQuery(orderId ? orderId : '');
-  const [updateOrderStatus] = useUpdateOrderStatusMutation();
-  const [updateOrderShipping] = useUpdateOrderShippingMutation();
-  const [status, setStatus] = useState<OrderStatus | ''>(order?.status || '');
-  const [shippingMethod, setShippingMethod] = useState<ShippingMethod | ''>(
-    order?.shippingMethod || ''
-  );
+  const {order, isLoading, isError} = useOrder(useParams().orderId || "");
 
   if (isLoading) return <p>Loading Order...</p>;
   if (isError || !order) return <p>Order not found.</p>;
-
-  const handleBackBtn = () => {
-    navigate(-1);
-  };
-
-  const handleStatusChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setStatus(e.target.value as OrderStatus);
-  };
-
-  const handleShippingMethodChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setShippingMethod(e.target.value as ShippingMethod);
-  };
-
-  const handleStatusSave = async () => {
-    try {
-      if (orderId && status && status != order.status) {
-        await updateOrderStatus({ orderId, status }).unwrap();
-      }
-
-      if (orderId && shippingMethod && shippingMethod != order.shippingMethod) {
-        await updateOrderShipping({ orderId, shippingMethod }).unwrap();
-      }
-
-      alert('Order status has been updated successfully');
-    } catch (error) {
-      alert('Failed to update order status');
-    }
-  };
-
+ 
   return (
-    <article className="order">
-      <h2 className="orders-header">Order Details</h2>
-      <main>
-        <h3>Order No: {order.orderNo}</h3>
-        <div className="order-field">
-          <p>Created on:</p>
-          <p>{new Date(order.created_at).toLocaleDateString()}</p>
-          <p>Order status:</p>
-          <p>{order.status.toUpperCase()}</p>
-          <p>Shipping Method:</p>
-          <p>
-            {order.shippingMethod
-              ? order.shippingMethod.toUpperCase()
-              : 'NO DATA'}
-          </p>
-          <p>Paid status:</p>
-          <p
-            className={
-              order.isPaid ? 'order-status.paid' : 'order-status.not-paid'
-            }
-          >
-            {order.isPaid ? 'Paid' : 'Not Paid'}
-          </p>
-        </div>
-        <h3>Order Details</h3>
-
-        <OrderProducts products={order.products} />
+    <article className="checkout">
+      <h2 className="header-wraper">Order No: {order.orderNo}</h2>
+      <OrderSummary order={order}/>
+      <OrderProducts products={order.products} />
+      <div className="cart-bottom">
+        <Address address={order.shippingAddress} />
         <OrderPriceSummary
           itemsPrice={order.itemsPrice}
           shippingPrice={order.shippingPrice}
-          totalPrice={order.totalPrice}
         />
-        <Address address={order.shippingAddress} />
-
-        {/* Admin-only tools */}
-        {isAdmin && (
-          <section className="admin-tools">
-            <h3>Admin Tools</h3>
-
-            <div>
-              <label htmlFor="status">Update Status:</label>
-              <select id="status" value={status} onChange={handleStatusChange}>
-                {Object.values(OrderStatus).map((status) => (
-                  <option key={status} value={status}>
-                    {status.charAt(0).toUpperCase() + status.slice(1)}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <div>
-              <label htmlFor="shippingMethod">Update Shipping Method:</label>
-              <select
-                id="shippingMethod"
-                value={shippingMethod}
-                onChange={handleShippingMethodChange}
-              >
-                {Object.values(ShippingMethod).map((method) => (
-                  <option key={method} value={method}>
-                    {method.charAt(0).toUpperCase() + method.slice(1)} Shipping
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <button onClick={handleStatusSave}>Save Changes</button>
-          </section>
-        )}
-      </main>
-      <div className="buttons-box">
-        <button className="btn back-btn" onClick={handleBackBtn}>
-          Go Back
-        </button>
       </div>
+      <OrderActions order={order} />
     </article>
   );
 };
