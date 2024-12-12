4809e25907ea11fef0efbc7b855ae43ee0b7fcf2
Martin Justa <Aztabanee@gmail.com>
2024-12-12 03:09:29 +0100

    refactor: added OrderPriceSummary component.

--- src/components/checkout/Cart.tsx
+++ src/components/checkout/Cart.tsx
@@ -1,49 +1,50 @@
 import { useSelector } from 'react-redux';
 import { ReactElement } from 'react';
 import CartLineItem from './CartLineItem';
-import { selectCartItems } from '../../features/checkout/checkoutSlice';
-import { eurFormat, countTaxFree } from '../../utils/utils';
+import {
+  selectCartItems,
+  selectShippingMethod,
+} from '../../features/checkout/checkoutSlice';
+import { calculateOrderPrices } from '../../utils/utils';
 import { NavLink } from 'react-router-dom';
+import OrderPriceSummary from '../orders/OrderPriceSummary';
 
 const Cart = (): ReactElement => {
   const products = useSelector(selectCartItems);
-
-  const totalItems = products.reduce((total, item) => total + item.quantity, 0);
-  const itemsPrice = products.reduce(
-    (total, item) => total + item.price * item.quantity,
-    0
+  const shippingMethod = useSelector(selectShippingMethod);
+  const { itemsPrice, shippingPrice } = calculateOrderPrices(
+    products,
+    shippingMethod
   );
 
   const pageContent = (
     <>
       {products.length ? (
-        <>
+        <div className="checkout checkout">
           <ul className="cart">
             {products.map((item) => {
               return <CartLineItem key={item.product} item={item} />;
             })}
           </ul>
-          <div className="cart-summary">
-            <label>Total Items: </label>
-            <p>{totalItems}</p>
-            <label>Without Tax: </label>
-            <p>{eurFormat(countTaxFree(itemsPrice))}</p>
-            <label>Total Price: </label>
-            <p>{eurFormat(itemsPrice)}</p>
+          <div className="cart-bottom">
+            <OrderPriceSummary
+              itemsPrice={itemsPrice}
+              shippingPrice={shippingPrice}
+            />
           </div>
-        </>
+        </div>
       ) : (
-        <>
+        <div className="checkout checkout-spaced">
           <p>No Items in cart</p>
           <p>
-            Visit our <NavLink to='/shop'>SHOP</NavLink>
+            Visit our <NavLink to="/shop">SHOP</NavLink>
           </p>
-        </>
+        </div>
       )}
     </>
   );
 
-  return <div className="checkout-main">{pageContent}</div>;
+  return pageContent;
 };
 
 export default Cart;
