6812f266de30402cc70383712bfb5c34a6b7d70d
Martin Justa <Aztabanee@gmail.com>
2025-01-20 18:21:14 +0100

    refactor: added Pay Now button on unpaid orders.

--- src/components/orders/SingleOrderPage.tsx
+++ src/components/orders/SingleOrderPage.tsx
@@ -1,21 +1,30 @@
-import { useParams } from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
 import useOrder from '../../hooks/useOrder';
 import OrderProducts from './OrderProducts';
 import OrderSummary from './OrderSummary';
 import OrderPriceSummary from './OrderPriceSummary';
 import Address from './Address';
 import OrderActions from '../admin/orders/OrderActions';
+import useAuth from '../../hooks/useAuth';
 
 const SingleOrderPage = () => {
-  const {order, isLoading, isError} = useOrder(useParams().orderId || "");
+  const orderId = useParams().orderId  || '';
+  const { order, isLoading, isError } = useOrder(orderId);
+  const { isAdmin } = useAuth();
 
   if (isLoading) return <p>Loading Order...</p>;
   if (isError || !order) return <p>Order not found.</p>;
- 
+
   return (
     <article className="checkout">
-      <h2 className="header-wraper">Order No: {order.orderNo}</h2>
-      <OrderSummary order={order}/>
+      <h2 className="header-wraper">
+        <p>Order No: {order.orderNo}</p>
+        {!isAdmin && !order.isPaid && (
+          <button className="btn back-btn"><NavLink to={`/checkout/payment/${orderId}`}>Pay now</NavLink></button>
+        )}
+      </h2>
+
+      <OrderSummary order={order} />
       <OrderProducts products={order.products} />
       <div className="cart-bottom">
         <Address address={order.shippingAddress} />
