186989ad97dd841b2f8628306610fd56e2d65c94
Martin Justa <Aztabanee@gmail.com>
2025-01-31 00:20:20 +0100

    feat:  added functional  search bar

--- src/components/admin/orders/AdminOrdersPage.tsx
+++ src/components/admin/orders/AdminOrdersPage.tsx
@@ -1,51 +1,24 @@
-import { useState, useMemo, ChangeEvent, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectOrders, setOrders } from '../../../features/shop/ordersSlice';
-import {
-  Order,
-  useGetAllOrdersQuery,
-} from '../../../features/shop/ordersApiSlice';
-import { ShippingMethod } from '../../../config/shippingConfig';
+import { useState, useMemo, ChangeEvent} from 'react';
+import { useSelector } from 'react-redux';
+import { selectOrders } from '../../../features/shop/ordersSlice';
+import { Order } from '../../../features/shop/ordersApiSlice';
 import AdminOrderList from './AdminOrderList';
 import Pagination from '../../common/Pagination';
 
 const AdminOrdersPage = () => {
-  const dispatch = useDispatch();
-  const { data: ordersData, refetch } = useGetAllOrdersQuery();
   const orders = useSelector(selectOrders);
 
   const [search, setSearch] = useState<string>('');
   const [page, setPage] = useState<number>(1);
-  const [shippingMethod, setShippingMethod] = useState<ShippingMethod | ''>('');
-  const [isPaid, setIsPaid] = useState<string>('');
-  const itemsPerPage = 10;
-
-  useEffect(() => {
-    if (ordersData) {
-      dispatch(setOrders(ordersData));
-    } else {
-      refetch();
-    }
-  }, [ordersData, dispatch, refetch]);
+  const itemsPerPage = 15;
 
   // Filter orders based on search input
-  const filteredOrders = useMemo<Order[]>(() => {
-    return orders.filter((order) => {
-      const matchesSearch =
-        order.user.username.toLowerCase().includes(search.toLowerCase()) ||
-        order.orderNo.toString().includes(search);
-      const matchesShipping = shippingMethod
-        ? order.shippingMethod === shippingMethod
-        : true;
-      const matchesPayment =
-        isPaid === 'paid'
-          ? order.isPaid
-          : isPaid === 'notPaid'
-          ? !order.isPaid
-          : true;
-      return matchesSearch && matchesShipping && matchesPayment;
-    });
-  }, [orders, search, shippingMethod, isPaid]);
+  const filteredOrders = useMemo(() => {
+    return orders.filter((order) =>
+      order.orderNo.toString().toLowerCase().includes(search.toLowerCase()) ||
+      order.user.username.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [orders, search]);
 
   // Calculate paginated order from current page
   const paginatedOrders = useMemo<Order[]>(() => {
@@ -61,17 +34,6 @@ const AdminOrdersPage = () => {
     setSearch(e.target.value);
     setPage(1);
   };
-
-  const handleShippingMethodChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setShippingMethod(e.target.value as ShippingMethod);
-    setPage(1);
-  };
-
-  const handleIsPaidChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setIsPaid(e.target.value);
-    setPage(1);
-  };
-
   // Handle page change
   const handlePageChange = (newPage: number) => {
     setPage(newPage);
@@ -79,32 +41,16 @@ const AdminOrdersPage = () => {
 
   return (
     <article className="orders-main-page">
-      <h2 className="header-wraper">Admin Orders</h2>
-      <div>
+      <h2 className="header-wraper">
+        Admin Orders{' '}
         <input
           type="text"
-          placeholder="Search by username or order ID"
+          placeholder="username or order ID"
           value={search}
           onChange={handleSearch}
           className="search-bar"
         />
-
-        <select value={shippingMethod} onChange={handleShippingMethodChange}>
-          <option value="">All Shipping Methods</option>
-          {Object.values(ShippingMethod).map((method) => (
-            <option key={method} value={method}>
-              {method.charAt(0).toUpperCase() + method.slice(1)} Shipping
-            </option>
-          ))}
-        </select>
-
-        <select value={isPaid} onChange={handleIsPaidChange}>
-          <option value="">All orders</option>
-          <option value="paid">Paid</option>
-          <option value="notPaid">Not Paid</option>
-        </select>
-      </div>
-
+      </h2>
       <AdminOrderList orders={paginatedOrders} />
       <Pagination
         currentPage={page}
