49529ea2c38b3257535b477dda247d104b014d8b
Martin Justa <Aztabanee@gmail.com>
2025-01-31 01:02:47 +0100

    feat: added sortable heders.

--- src/components/admin/orders/AdminOrderList.tsx
+++ src/components/admin/orders/AdminOrderList.tsx
@@ -1,30 +1,51 @@
 import { ReactElement } from 'react';
 import { Order } from '../../../features/shop/ordersApiSlice';
 import AdminOrderLineItem from './AdminOrderLineItem';
+import { useSorting } from '../../../hooks/useSorting';
+import SortableHeader from '../SortableHeader';
+
 
 interface AdminOrderListProps {
   orders: Order[];
 }
 
 const AdminOrderList = ({ orders }: AdminOrderListProps): ReactElement => {
+
+    
+  
+  const { sortedData, sortConfig, handleSort } = useSorting(orders);
+
   if (!orders || orders.length === 0) {
     return <p>No orders to found.</p>;
   }
 
   return (
-    <table className="admin-orders-list">
+    <table className="admin-products-list">
       <thead>
-        <tr className="admin-order-line-header">
+        <tr>
           <th>Order Number</th>
-          <th>Created</th>
+          <SortableHeader
+            label="created"
+            sortKey="created_at"
+            currentSortKey={sortConfig.key}
+            currentSortDirection={sortConfig.direction}
+            onSort={handleSort}
+          />
           <th>User</th>
-          <th>Shipping Method</th>
+          <th>Shipping</th>
+          <th>Status</th>
           <th>Paid status</th>
-          <th className="order-price">Total Price</th>
+          <SortableHeader
+            label="total"
+            sortKey="totalPrice"
+            currentSortKey={sortConfig.key}
+            currentSortDirection={sortConfig.direction}
+            onSort={handleSort}
+          />{' '}
         </tr>
       </thead>
       <tbody>
-        {orders.map((order) => (
+        {sortedData.map((order) => (
           <AdminOrderLineItem key={order.id} order={order} />
         ))}
       </tbody>
