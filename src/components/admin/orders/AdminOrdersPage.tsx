2dc7bc17a6734d852ef2f761d2a4e8b3df06b1e3
Martin Justa <Aztabanee@gmail.com>
2025-01-31 01:03:27 +0100

    refactor: data noew from rtk query request instead of redux state

--- src/components/admin/orders/AdminOrdersPage.tsx
+++ src/components/admin/orders/AdminOrdersPage.tsx
@@ -1,15 +1,12 @@
 import { useState, useMemo, ChangeEvent} from 'react';
-import { useSelector } from 'react-redux';
-import { selectOrders } from '../../../features/shop/ordersSlice';
-import { Order } from '../../../features/shop/ordersApiSlice';
+import { useGetAllOrdersQuery } from '../../../features/shop/ordersApiSlice';
 import AdminOrderList from './AdminOrderList';
 import Pagination from '../../common/Pagination';
 
 const AdminOrdersPage = () => {
-  const orders = useSelector(selectOrders);
+  const { data: orders  = [], isError, isLoading } = useGetAllOrdersQuery();
 
   const [search, setSearch] = useState<string>('');
-  const [page, setPage] = useState<number>(1);
   const itemsPerPage = 15;
 
   // Filter orders based on search input
@@ -20,24 +17,14 @@ const AdminOrdersPage = () => {
     );
   }, [orders, search]);
 
-  // Calculate paginated order from current page
-  const paginatedOrders = useMemo<Order[]>(() => {
-    const start = (page - 1) * itemsPerPage;
-    return filteredOrders.slice(start, start + itemsPerPage);
-  }, [filteredOrders, page, itemsPerPage]);
-
-  // Calculate total pages based on filtered orders
-  const totalPages = Math.ceil(filteredOrders.length / itemsPerPage);
-
   // Update search and reset to page 1 when search input changes
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
-    setPage(1);
   };
   // Handle page change
-  const handlePageChange = (newPage: number) => {
-    setPage(newPage);
-  };
+
+  if (isLoading) return <p>Loading orders...</p>;
+  if (isError) return <p>Error loading orders.</p>;
 
   return (
     <article className="orders-main-page">
@@ -51,11 +38,10 @@ const AdminOrdersPage = () => {
           className="search-bar"
         />
       </h2>
-      <AdminOrderList orders={paginatedOrders} />
       <Pagination
-        currentPage={page}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
+        data={filteredOrders}
+        itemsPerPage={itemsPerPage}
+        render={(paginatedData) => <AdminOrderList orders={paginatedData} />}
       />
     </article>
   );
