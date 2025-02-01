3a9f310dc237cd3a71a1ee2770e6a4261de41920
Martin Justa <Aztabanee@gmail.com>
2025-02-01 04:38:18 +0100

    fix: category filter changed to memo for better performance.

--- src/hooks/useCategoryFilter.ts
+++ src/hooks/useCategoryFilter.ts
@@ -1,20 +1,17 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export const useCategoryFilter = <T>(data: T[], categoryKey: keyof T) => {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-  const [filteredData, setFilteredData] = useState<T[]>(data);
+  const filteredData = useMemo(() => {
+    if (selectedCategories.length === 0) return data;
+
+    return data.filter((item) =>
+      selectedCategories.includes(item[categoryKey] as string)
+    );
+  }, [data, selectedCategories, categoryKey]);
 
   const handleCategoryFilterChange = (updatedCategories: string[]) => {
     setSelectedCategories(updatedCategories);
-
-    if (updatedCategories.length === 0) {
-      setFilteredData(data);
-    } else {
-      const filtered = data.filter((item) =>
-        updatedCategories.includes(item[categoryKey] as string)
-      );
-      setFilteredData(filtered);
-    }
   };
 
   return { filteredData, selectedCategories, handleCategoryFilterChange };
