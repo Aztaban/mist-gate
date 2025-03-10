ce5132c3a2ef771b80e5d20b57df7b9cdce90a96
Martin Justa <Aztabanee@gmail.com>
2025-03-11 00:41:31 +0100

    fix: added new css script in main.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './styles/index.css';
+import './styles/main.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './features/store.ts';
