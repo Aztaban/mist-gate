f2cc13c817a747fd0e425210de922e76c38d95d4
Martin Justa <Aztabanee@gmail.com>
2025-03-11 22:43:29 +0100

    refactor: orders sorted before sent to OrdersList

--- src/components/auth/Account.tsx
+++ src/components/auth/Account.tsx
@@ -6,13 +6,17 @@ import UserSettings from '../user/UserSettings';
 
 const Account = (): ReactElement => {
   const { username } = useAuth();
-  const { data: orders = [] } = useGetOrdersForUserQuery();
+  const { data: orders } = useGetOrdersForUserQuery();
+
+  const sortedOrders = [...orders ?? []].sort((a, b) => 
+    new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+  );
 
   return (
     <article className="checkout-main">
       <h2 className="header-wraper">{username}'s Account</h2>
-      <OrdersList orders={orders}/>
-      <h2>User Settings</h2>
+      <OrdersList orders={sortedOrders}/>
+      <h2 className="header-wraper">User Settings</h2>
       <UserSettings />
     </article>
   );
--- src/components/user/UserInfo.tsx
+++ src/components/user/UserInfo.tsx
@@ -1,5 +1,5 @@
-import { ReactElement } from "react";
-import { User } from "../../types";
+import { ReactElement } from 'react';
+import { User } from '../../types';
 
 interface UserInfoProps {
   user: User;
@@ -8,25 +8,25 @@ interface UserInfoProps {
 const UserInfo = ({ user }: UserInfoProps): ReactElement => {
   return (
     <div className="user-info">
-    <label>Username:</label>
-    <p>{user.username}</p>
+      <label>Username:</label>
+      <p>{user.username}</p>
 
-    <label>Email:</label>
-    <p>{user.email}</p>
-    <button>Change Email</button>
-    <div></div>
+      <label>Email:</label>
+      <p>{user.email}</p>
+      <button>Change Email</button>
+      <div></div>
 
-    <label>Telephone Number:</label>
-    <p>{user.phoneNumber}</p>
+      <label>Telephone Number:</label>
+      <p>{user.phoneNumber}</p>
 
-    <button>Change Number</button>
-    <div></div>
+      <button>Change Number</button>
+      <div></div>
 
-    <label>Password:</label>
-    <p>********</p>
-    <button>Change Password</button>
-  </div>
-  );  
-}
+      <label>Password:</label>
+      <p>********</p>
+      <button>Change Password</button>
+    </div>
+  );
+};
 
-export default UserInfo
+export default UserInfo;
