8579c0b0f4c6f4c8f0043c9be7cd2f363a8e99a6
Martin Justa <Aztabanee@gmail.com>
2025-04-02 00:28:01 +0200

    refactor: upload image hook has been modified with input check.

--- src/hooks/ui/useUploadImage.ts
+++ src/hooks/ui/useUploadImage.ts
@@ -1,14 +1,61 @@
-import { useUploadImageMutation } from "@features/apiSlices/productApiSlice";
+import { useState } from 'react';
 
-export const uploadImageAndGetPath = async (
-  file: File,
-  uploadImage: ReturnType<typeof useUploadImageMutation>[0]
-): Promise<string> => {
-  try {
-    const { image: uploadedImagePath } = await uploadImage(file).unwrap();
-    console.log('Image uploaded: ', uploadedImagePath);
-    return uploadedImagePath;
-  } catch (error) {
-    throw new Error('Image upload failed');
-  }
+interface UseImageUploadOptions {
+  maxSizeMb?: number;
+  minWidth?: number;
+  minHeight?: number;
+}
+
+export const useImageUpload = ({
+  maxSizeMb = 2,
+  minWidth = 300,
+  minHeight = 300,
+}: UseImageUploadOptions = {}) => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const objectUrl = URL.createObjectURL(file);
+
+    if (file.size > maxSizeMb * 1024 * 1024) {
+      setError(`Image size exceeds ${maxSizeMb}MB`);
+      setSelectedFile(null);
+      setPreviewUrl(null);
+      URL.revokeObjectURL(objectUrl);
+      return;
+    }
+
+    const img = new Image();
+
+    img.src = objectUrl;
+
+    img.onload = () => {
+      if (img.width < minWidth || img.height < minHeight) {
+        setError(
+          `Image dimensions must be at least ${minWidth}x${minHeight}px`
+        );
+        URL.revokeObjectURL(objectUrl);
+        setSelectedFile(null);
+        setPreviewUrl(null);
+        return;
+      }
+      setSelectedFile(file);
+      setPreviewUrl(objectUrl);
+      setError(null);
+    };
+    img.onerror = () => {
+      setError('Invalid image file.');
+    };
+  };
+  const reset = () => {
+    setSelectedFile(null);
+    setPreviewUrl(null);
+    setError(null);
+  };
+
+  return { selectedFile, previewUrl, error, handleFileChange, reset };
 };
