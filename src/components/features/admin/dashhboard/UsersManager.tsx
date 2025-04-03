b04b8dc87b498cbb9762dd95018274fa6521d573
Martin Justa <Aztabanee@gmail.com>
2025-04-03 02:44:21 +0200

    Feat: created basic styles and components for  Users manager.

--- src/components/features/admin/dashhboard/UsersManager.tsx
+++ src/components/features/admin/dashhboard/UsersManager.tsx
@@ -1,33 +1,46 @@
-import { useGetAllUsersQuery } from "@features/apiSlices/userApiSlice"
-import { useState } from "react";
+import { useGetAllUsersQuery } from '@features/apiSlices/userApiSlice';
+import { useState } from 'react';
+import UserCard from './UserCard';
 
 const UsersManager = () => {
   const { data = [], isLoading, isError } = useGetAllUsersQuery();
   const [search, setSearch] = useState<string>('');
+  const [selectedUserId, setSelectedUserId] = useState<string>('');
 
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error loading data.</p>;
 
-  const filteredUsers = data.filter((user) =>
-    user.username?.toLowerCase().includes(search.toLowerCase())
-  );
+  const filteredUsers = data.filter((user) => user.username?.toLowerCase().includes(search.toLowerCase()));
 
   return (
-    <div>
+    <div className="users-manager">
       <h2>Users Manager</h2>
-      <input type="text" placeholder="Search users" value={search} onChange={(e) => setSearch(e.target.value)} />
+      <div className="users-grid">
+        <div className="search-user">
+          <input
+            type="text"
+            placeholder="Search users"
+            className="search-bar"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          {filteredUsers.length === 0 || search === '' ? (
+            <p>No users found.</p>
+          ) : (
+            <ul>
+              {filteredUsers.slice(0, 5).map((user) => (
+                <li key={user.id} onClick={() => setSelectedUserId(user.id)}>
+                  {user.username}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
 
-      {filteredUsers.length === 0 ? (
-        <p>No users found.</p>
-      ) : (
-        <ul>
-          {filteredUsers.slice(0, 5).map((user) => (
-            <li key={user.id}>{user.username}</li>
-          ))}
-        </ul>
-      )}
+        {selectedUserId && <UserCard userId={selectedUserId} />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UsersManager
+export default UsersManager;
