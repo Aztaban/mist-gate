4079b7a687a5d03ea192a67eb34e5b0d85c4c88c
Aztaban <Aztabanee@gmail.com>
2025-05-12 23:45:31 +0200

    fix: Phone modal import

--- src/components/features/user/UserInfo.tsx
+++ src/components/features/user/UserInfo.tsx
@@ -2,16 +2,16 @@ import { ReactElement, useState } from 'react';
 import { User } from '@types';
 import PasswordChangeModal from '../auth/modals/PasswordChangeModal';
 import EmailChangeModal from '../auth/modals/EmailChangeModal';
-import PhoneNumberChangeModal from '../auth/modals/phoneNumberChangeModal';
+import PhoneNumberChangeModal from '../auth/modals/PhoneNumberChangeModal';
 
 interface UserInfoProps {
   user: User;
 }
 
-type ModalType = "password" | "email" | "phone" | null;
+type ModalType = 'password' | 'email' | 'phone' | null;
 
 const UserInfo = ({ user }: UserInfoProps): ReactElement => {
-  const [openModal, setOpenModal] = useState<ModalType>(null); 
+  const [openModal, setOpenModal] = useState<ModalType>(null);
   return (
     <div className="user-info">
       <label>Username:</label>
@@ -19,22 +19,24 @@ const UserInfo = ({ user }: UserInfoProps): ReactElement => {
 
       <label>Email:</label>
       <p>{user.email}</p>
-      <button onClick={() => setOpenModal("email")}>Change Email</button>
+      <button onClick={() => setOpenModal('email')}>Change Email</button>
       <div></div>
 
       <label>Telephone Number:</label>
       <p>{user.phoneNumber}</p>
 
-      <button onClick={() => setOpenModal("phone")}>Change Number</button>
+      <button onClick={() => setOpenModal('phone')}>Change Number</button>
       <div></div>
 
       <label>Password:</label>
       <p>********</p>
-      <button onClick={() => setOpenModal("password")}>Change Password</button>
+      <button onClick={() => setOpenModal('password')}>Change Password</button>
 
-      {openModal === "password" && <PasswordChangeModal onClose={() => setOpenModal(null)} />}
-      {openModal === "email" && <EmailChangeModal email={user.email} onClose={() => setOpenModal(null)} />}
-      {openModal === "phone" && <PhoneNumberChangeModal currentPhoneNumber={user.phoneNumber || ""} onClose={() => setOpenModal(null)} />}
+      {openModal === 'password' && <PasswordChangeModal onClose={() => setOpenModal(null)} />}
+      {openModal === 'email' && <EmailChangeModal email={user.email} onClose={() => setOpenModal(null)} />}
+      {openModal === 'phone' && (
+        <PhoneNumberChangeModal currentPhoneNumber={user.phoneNumber || ''} onClose={() => setOpenModal(null)} />
+      )}
     </div>
   );
 };
